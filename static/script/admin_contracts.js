e99cbb74b2058062dc6a381380611dd711960f5a
Dandee Galang <dandeegalang.ph@gmail.com>
2025-05-15 00:04:36 +0800

    fix: update currency formatting in admin contracts script; change from USD to PHP for accurate financial representation

--- static/script/admin_contracts.js
+++ static/script/admin_contracts.js
@@ -158,9 +158,9 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // Format currency
     function formatCurrency(amount) {
-        return new Intl.NumberFormat('en-US', {
+        return new Intl.NumberFormat('en-PH', {
             style: 'currency',
-            currency: 'USD'
+            currency: 'PHP'
         }).format(amount);
     }
     
